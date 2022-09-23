c73e80b5f00b91baf09dd28778b66445bd8b6ba6
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-09-23 17:05:46 +0530

    [Darshan]: add uc 5 get records from table using where and between filters

--- EmployeePayRollDB/EmployeePayRoll.sql
+++ EmployeePayRollDB/EmployeePayRoll.sql
@@ -13,4 +13,9 @@ Create table employee_payroll (
 -----------UC3-------------
 insert into employee_payroll values('Darshan',10000,'2020-11-13'),('Peter',15000.45,'2015-08-05'),('Tony',50000,GETDATE());
 -----------UC4--------
- select * from employee_payroll
+ select * from employee_payroll;
+ ----------UC5---------
+SELECT * FROM employee_payroll where  name = 'Darshan';
+SELECT * FROM employee_payroll where StartDate between '2018-08-15' and '2020-10-15';
+SELECT * FROM employee_payroll where StartDate between '2018-08-15' and GETDATE();
+SELECT * FROM employee_payroll where StartDate between cast ('05-15-2015' as date) and '2022-10-15';

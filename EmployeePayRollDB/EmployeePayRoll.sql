ec81baa26fc56d2cb3a6dc9d06c02a530d49bc9b
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-09-23 17:01:25 +0530

    [Darshan]: add uc-4 select query to read from database

--- EmployeePayRollDB/EmployeePayRoll.sql
+++ EmployeePayRollDB/EmployeePayRoll.sql
@@ -12,3 +12,5 @@ Create table employee_payroll (
 
 -----------UC3-------------
 insert into employee_payroll values('Darshan',10000,'2020-11-13'),('Peter',15000.45,'2015-08-05'),('Tony',50000,GETDATE());
+-----------UC4--------
+ select * from employee_payroll

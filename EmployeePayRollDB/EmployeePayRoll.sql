b0e0ec79d4e408929520f3396f228fd8b0f88cff
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-10-01 11:58:04 +0530

    [Darhan]: add uc-11 created tables as per ER diagram

--- EmployeePayRollDB/EmployeePayRoll.sql
+++ EmployeePayRollDB/EmployeePayRoll.sql
@@ -55,3 +55,91 @@ Alter table employee_payroll add Deductions float,Taxable_Pay float,Income_Tax f
 update employee_payroll set Department = 'Sales' where Name='Terissa';
 insert into  employee_payroll (Name,BasicPay,StartDate,Gender,PhoneNumber,Address,Department)
 Values('Terissa', 10500.23, '2017-08-17','F',9123456780,'Mumbai','Marketing');
+
+-----------------------UC11------------------------------------------------
+Create Table Company
+(CompanyID int identity(1,1) primary key,
+CompanyName varchar(50));
+
+--Insert Values in Company
+Insert into Company values ('Tesla'),('SpaceX')
+Select * from Company
+
+drop table employee_payroll;
+
+--Create Employee Table
+create table Employee
+(EmployeeID int identity(1000,1) primary key,
+CompanyIdentity int,
+EmployeeName varchar(50),
+EmployeePhoneNumber bigInt,
+EmployeeAddress varchar(100),
+StartDate date,
+Gender char,
+Foreign key (CompanyIdentity) references Company(CompanyID)
+)
+
+--Insert Values in Employee
+insert into Employee values
+(1,'Darshan Deshmukh',9995905050,'street 123','2012-03-28','M'),
+(2,'Light Yagami',9842905550,'street 789 ,near abc','2017-04-22','M'),
+(1,'Angelina Jolie',7612905050,'Hollywood road','2015-08-22','F'),
+(2,'Gol D Roger',88129050000,'one piece,tokyo','2012-08-29','M')
+Select * from Employee
+
+
+--Create Payroll Table
+create table PayrollCalculate
+(BasicPay float,
+Deductions float,
+TaxablePay float,
+IncomeTax float,
+NetPay float,
+EmployeeIdentity int,
+Foreign key (EmployeeIdentity) references Employee(EmployeeID)
+)
+
+--Insert Values in Payroll Table
+insert into PayrollCalculate(BasicPay,Deductions,IncomeTax,EmployeeIdentity) values 
+(5000000,10000,20000,1000),
+(4500000,8000,4000,1001),
+(6000000,10000,5000,1002),
+(9000000,399994,6784,1003)
+
+--Updating Derived attribute values of Payroll Table,TaxablePay and NetPay
+update PayrollCalculate set TaxablePay=BasicPay-Deductions
+update PayrollCalculate set NetPay=TaxablePay-IncomeTax
+select * from PayrollCalculate
+
+--Create Department Table
+create table Department
+(
+DepartmentId int identity(1,1) primary key,
+DepartName varchar(20)
+)
+
+--Insert Values in Department Table
+insert into Department values
+('Marketing'),
+('Sales'),
+('Publishing')
+select * from Department
+
+
+--Create table EmployeeDepartment table
+create table EmployeeDepartment
+(
+DepartmentIdentity int ,
+EmployeeIdentity int,
+Foreign key (EmployeeIdentity) references Employee(EmployeeID),
+Foreign key (DepartmentIdentity) references Department(DepartmentID)
+)
+
+--Insert Values in EmployeeDepartment
+insert into EmployeeDepartment values
+(3,1000),
+(2,1001),
+(1,1002),
+(3,1003)
+
+select * from EmployeeDepartment;
